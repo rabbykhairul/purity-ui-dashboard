d44daad2cde09bdd5fd9f712686fa05078a5c753
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 14:59:34 +0600

    SignUpForm - render input fields

--- src/components/forms/SignUpForm.jsx
+++ src/components/forms/SignUpForm.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Button from "../commons/Button";
+import Input from "../commons/Input";
 
 const SignUpForm = () => {
-  return (
-    <form className="sign-up-form">
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="sign-up-form" onSubmit={handleSubmit}>
+      <Input title="Name" placeholder="Your full name" name="fullName" />
+      <Input title="Email" placeholder="Your email address" name="email" type="email" />
+      <Input title="Password" placeholder="Your password" name="password" type="password" />
+      <Button type="submit" onClick={handleSubmit} >Sign Up</Button>
     </form>
   )
 };
