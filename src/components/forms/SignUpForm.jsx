d0dbc01c1e094ba7a11209f53d0608dfe1c7072f
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 15:18:40 +0600

    SignUpForm - render alternate registration option's title

--- src/components/forms/SignUpForm.jsx
+++ src/components/forms/SignUpForm.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import Button from "../commons/Button";
 import Input from "../commons/Input";
 
+const FORM_TITLE = "Register with";
+
 const SignUpForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
   }
 
+  const renderAlternateRegistrationOptions = () => {
+    return (
+      <>
+      <h2 className="color-gray-700">{FORM_TITLE}</h2>
+      <p className="color-gray-700">or</p>
+      </>
+    )
+  }
+
   return (
     <form className="sign-up-form" onSubmit={handleSubmit}>
+      {renderAlternateRegistrationOptions()}
       <Input title="Name" placeholder="Your full name" name="fullName" />
       <Input title="Email" placeholder="Your email address" name="email" type="email" />
       <Input title="Password" placeholder="Your password" name="password" type="password" />
