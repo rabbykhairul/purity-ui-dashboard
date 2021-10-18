4dc65d8aa80edce9a52ad0360ad207122df5f0d4
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 23:50:30 +0600

    create 'SignInScreen' component

---
+++ src/components/SignInScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageFooter from "./commons/PageFooter";
+
+const SignInScreen = () => {
+  return (
+    <div className="page-container sign-in-screen">
+      <PageFooter className="footer-with-padding" />
+    </div>
+  )
+}
+
+export default SignInScreen;
