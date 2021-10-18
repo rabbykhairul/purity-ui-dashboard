22906bca487d9ac046b117349d57db4c3db51e98
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 12:38:44 +0600

    SignUpScreen - add welcome note section contents

--- src/components/SignUpScreen.jsx
+++ src/components/SignUpScreen.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+const WELCOME_TITLE = "Welcome!";
+const WELCOME_MESSAGE = "Use these awesome forms to login or create new account in your project for free."
+
 const SignUpScreen = () => {
 
   const renderWelcomeNoteSection = () => {
-    return <div className="welcome-note-box" style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/themed-background.png')`}}></div>;
+    return (
+      <div className="welcome-note-box color-white" style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/themed-background.png')`}}>
+        <h1>{WELCOME_TITLE}</h1>
+        <p>{WELCOME_MESSAGE}</p>
+      </div>
+    );
   }
 
   return (
