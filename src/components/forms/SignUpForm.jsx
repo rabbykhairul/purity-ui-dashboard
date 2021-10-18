e9253f27179cf222607d0e6450fa6362943d445e
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 22:59:34 +0600

    SignUpForm - render form footer

--- src/components/forms/SignUpForm.jsx
+++ src/components/forms/SignUpForm.jsx
@@ -32,6 +32,12 @@ const SignUpForm = () => {
     )
   }
 
+  const renderFormFooter = () => {
+    return (
+      <p className="form-footer color-gray-400">Already have an account? <span className="color-teal">Sign in</span></p>
+    )
+  }
+
   return (
     <form className="sign-up-form" onSubmit={handleSubmit}>
       {renderAlternateRegistrationOptions()}
@@ -39,6 +45,7 @@ const SignUpForm = () => {
       <Input title="Email" placeholder="Your email address" name="email" type="email" />
       <Input title="Password" placeholder="Your password" name="password" type="password" />
       <Button type="submit" onClick={handleSubmit} >Sign Up</Button>
+      {renderFormFooter()}
     </form>
   )
 };
