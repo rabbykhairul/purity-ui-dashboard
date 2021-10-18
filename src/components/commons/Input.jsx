939c051d6932502caedf96af4c9d5aa37c5f5d4b
rabbykhairul <rabbykhairul@gmail.com>
2021-10-18 14:58:37 +0600

    create 'Input' component

---
+++ src/components/commons/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Input = (props) => {
+  const { title = "", type = "text", name="", value = "", placeholder = "Enter some text", onChange = () => {} } = props;
+
+  return (
+    <>
+    <label htmlFor={name} className="color-gray-700">{title}</label>
+    <input type={type} name={name} placeholder={placeholder} onChange={onChange} id={name} className="color-gray-700" />
+    </>
+  )
+};
+
+export default Input;
