2477570e3c1d48cab1e86c7249f233a6fe1d5157
rabbykhairul <rabbykhairul@gmail.com>
2021-10-19 15:40:30 +0600

    create component 'MainPage'

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "./commons/TopBar";
+
+const MainPage = () => {
+
+  return (
+    <div className="page-container main-page bg-gray-light">
+      <TopBar />
+    </div>
+  )
+};
+
+export default MainPage;
