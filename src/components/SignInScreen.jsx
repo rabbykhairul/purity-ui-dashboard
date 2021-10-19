06fbcb7c3f3919d3ddf878ac5bb52d6238e32942
rabbykhairul <rabbykhairul@gmail.com>
2021-10-19 10:39:55 +0600

    SignInScreen - render logo to the right side

--- src/components/SignInScreen.jsx
+++ src/components/SignInScreen.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import PageFooter from "./commons/PageFooter";
 
 const SignInScreen = () => {
+
+  const renderLogo = () => {
+    return (
+      <div className="logo-container">
+        <img src={`${process.env.PUBLIC_URL}/logo-big.png`} alt="" />
+      </div>
+    )
+  }
+
   return (
     <div className="page-container sign-in-screen">
-      <PageFooter className="footer-with-padding" />
+      {renderLogo()}
+      <PageFooter className="compact-footer" />
     </div>
   )
 }
