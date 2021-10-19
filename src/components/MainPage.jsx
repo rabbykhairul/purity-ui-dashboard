8c4b8521ef837001ea31a9db314ab95760741c27
rabbykhairul <rabbykhairul@gmail.com>
2021-10-19 20:15:01 +0600

    MainPage - render left side bar

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LeftSideBar from "./commons/LeftSideBar";
 import TopBar from "./commons/TopBar";
 
 const MainPage = () => {
@@ -6,6 +7,7 @@ const MainPage = () => {
   return (
     <div className="page-container main-page bg-gray-light">
       <TopBar />
+      <LeftSideBar />
     </div>
   )
 };
