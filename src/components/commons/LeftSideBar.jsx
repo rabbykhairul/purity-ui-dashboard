1706b5f7b5ad7dee1f5fbef0bd99416853058b78
rabbykhairul <rabbykhairul@gmail.com>
2021-10-19 21:37:41 +0600

    LeftSideBar - render menu items

--- src/components/commons/LeftSideBar.jsx
+++ src/components/commons/LeftSideBar.jsx
@@ -1,38 +1,41 @@
-import React from "react";
-import { IoHome } from "react-icons/io5";
-import { IoMdStats } from "react-icons/io";
-import { BsCreditCardFill } from "react-icons/bs";
-import { FaWrench } from "react-icons/fa";
+import React, { useState } from "react";
 import Button from "./Button";
+
+import { DASHBOARD_MENU, PROFILE_MENU } from "../../utils/menuItems";
 
 const LeftSideBar = () => {
 
-  const renderDashboardMenuItems = () => {
+  const [activeMenuItem] = useState(DASHBOARD_MENU.items[0]);
+
+  const renderMenuItems = (menuInfo) => {
+    const buttonClassActive = "active bg-white";
+    const buttonClassInActive = "bg-transparent";
+    const iconContainerClassActive = "bg-teal";
+    const iconContainerClassInActive = "bg-white";
+    const iconClassActive = "color-white";
+    const iconClassInActive = "color-teal";
+
+    const menuItems = menuInfo.items.map((item) => (
+      <Button key={item.id} className={`btn-menu-item color-gray-700 ${item.id === activeMenuItem.id ? buttonClassActive : buttonClassInActive}`}>
+        <span className={`menu-item-icon-container ${item.id === activeMenuItem.id ? iconContainerClassActive : iconContainerClassInActive}`}>
+          <item.icon className={`menu-item-icon ${item.id === activeMenuItem.id ? iconClassActive : iconClassInActive}`} />
+        </span>
+        {item.label}
+      </Button>
+    ));
+
     return (
       <>
-      <Button className="btn-menu-item active bg-white color-gray-700">
-        <span className="menu-item-icon-container bg-teal"><IoHome className="menu-item-icon color-white" /></span>
-        Dashboard
-      </Button>
-      <Button className="btn-menu-item bg-transparent color-gray-700">
-        <span className="menu-item-icon-container bg-white"><IoMdStats className="menu-item-icon color-teal" /></span>
-        Tables
-      </Button>
-      <Button className="btn-menu-item bg-transparent color-gray-700">
-        <span className="menu-item-icon-container bg-white"><BsCreditCardFill className="menu-item-icon color-teal" /></span>
-        Billing
-      </Button>
-      <Button className="btn-menu-item bg-transparent color-gray-700">
-        <span className="menu-item-icon-container bg-white"><FaWrench className="menu-item-icon color-teal" /></span>
-        RTL
-      </Button>
+      {menuInfo.title && <p className="menu-title color-gray-700">{menuInfo.title}</p>}
+      {menuItems}
       </>
     )
   }
 
   return (
     <nav className="left-side-bar">
-      {renderDashboardMenuItems()}
+      {renderMenuItems(DASHBOARD_MENU)}
+      {renderMenuItems(PROFILE_MENU)}
     </nav>
   )
 };
