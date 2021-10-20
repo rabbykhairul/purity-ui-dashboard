75769229cd6026528b0cefc29d3900cf5f630bd7
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 08:43:09 +0600

    LeftSideBar - make the menu items interactive

--- src/components/commons/LeftSideBar.jsx
+++ src/components/commons/LeftSideBar.jsx
@@ -3,10 +3,23 @@ import { BsFillQuestionCircleFill } from "react-icons/bs";
 import Button from "./Button";
 
 import { DASHBOARD_MENU, PROFILE_MENU } from "../../utils/menuItems";
+import { useHistory, useLocation } from "react-router";
 
 const LeftSideBar = () => {
+  const location = useLocation();
+  const history = useHistory();
 
-  const [activeMenuItem] = useState(DASHBOARD_MENU.items[0]);
+  const [activeMenuItem, setActiveMenuItem] = useState(computeActiveMenuItemFromURL());
+
+  function computeActiveMenuItemFromURL() {
+    const activeMenuItem = DASHBOARD_MENU.items.find(item => location.pathname.includes(item.path));
+    return activeMenuItem ?? DASHBOARD_MENU.items[0];
+  }
+
+  const handleRedirect = (menuItem) => {
+    setActiveMenuItem(menuItem);
+    history.push(menuItem.path);
+  }
 
   const renderMenuItems = (menuInfo) => {
     const buttonClassActive = "active bg-white";
@@ -17,7 +30,7 @@ const LeftSideBar = () => {
     const iconClassInActive = "color-teal";
 
     const menuItems = menuInfo.items.map((item) => (
-      <Button key={item.id} className={`btn-menu-item color-gray-700 ${item.id === activeMenuItem.id ? buttonClassActive : buttonClassInActive}`}>
+      <Button key={item.id} onClick={() => handleRedirect(item)} className={`btn-menu-item color-gray-700 ${item.id === activeMenuItem.id ? buttonClassActive : buttonClassInActive}`}>
         <span className={`menu-item-icon-container ${item.id === activeMenuItem.id ? iconContainerClassActive : iconContainerClassInActive}`}>
           <item.icon className={`menu-item-icon ${item.id === activeMenuItem.id ? iconClassActive : iconClassInActive}`} />
         </span>
