75f490e4e5a50856f38e6724bf65cbad953621a8
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 21:03:03 +0600

    DashboardScreen - render promo content 1

--- src/components/DashboardScreen.jsx
+++ src/components/DashboardScreen.jsx
@@ -1,11 +1,42 @@
 import React from "react";
-import PageFooter from "./commons/PageFooter";
+import { BsArrowRightShort } from "react-icons/bs";
+import Button from "./commons/Button";
 import ShortHighlights from "./statistics/ShortHighlights";
+import PageFooter from "./commons/PageFooter";
 
 const DashboardScreen = () => {
+
+  const renderPromotions = () => {
+    return (
+      <div className="promotions-container">
+        <div className="promotion-box-1 page-section bg-white">
+          <div className="info-container">
+            <div>
+              <p className="color-gray-400">Built by developers</p>
+              <p className="color-gray-700">Purity UI Dashboard</p>
+              <p className="color-gray-400">
+                From colors, cards, typography to complex elements, you will find the full documentation.
+              </p>
+            </div>
+            <Button className="btn-plain color-gray-700">
+              Read more <BsArrowRightShort className="icon-mini" />
+            </Button>
+          </div>
+          <div className="logo-container">
+            <img src={`${process.env.PUBLIC_URL}/logo-small.png`} alt="" />
+          </div>
+        </div>
+        <div className="promotion-box-2 page-section bg-white"></div>
+      </div>
+    )
+  }
+
   return (
     <div className="dashboard-screen">
-      <ShortHighlights />
+      <div>
+        <ShortHighlights />
+        {renderPromotions()}
+      </div>
       <PageFooter className="footer-with-vertical-gap" />
     </div>
   )
