c1c4fac96ffefb302273f6229a5405ea031c535e
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 09:15:44 +0600

    MainPage - render contents based on route path

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router";
 import LeftSideBar from "./commons/LeftSideBar";
 import TopBar from "./commons/TopBar";
 
 const MainPage = () => {
+  const location = useLocation();
+
+  const renderContents = () => {
+    if (location.pathname.includes("dashboard") || location.pathname === "/pages") return <p>Hello from dashboard!</p>;
+    else if (location.pathname.includes("tables")) return <p>Hello from tables!!</p>;
+
+    return null;
+  }
 
   return (
     <div className="page-container main-page bg-gray-light">
       <TopBar />
       <LeftSideBar />
+      {renderContents()}
     </div>
   )
 };
