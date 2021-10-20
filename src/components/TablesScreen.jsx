0b3b8563f2391ecb86d838cf10d035512e186471
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 15:18:31 +0600

    TablesScreen - render auther table with dummy data

--- src/components/TablesScreen.jsx
+++ src/components/TablesScreen.jsx
@@ -1,13 +1,49 @@
 import React from "react";
-import { renderAutherTableColumnHeaders, renderAutherTableHeader } from "../utils/tableHelper";
+import { renderAutherTableColumnHeaders, renderAutherTableHeader, renderAutherTableRow } from "../utils/tableHelper";
 import PageFooter from "./commons/PageFooter";
 import Table from "./tables/Table";
+
+export const authors = [
+  {
+    fullName: "Esthera Jackson",
+    email: "esthera@simmmple.com",
+    role: "Manager",
+    level: "Organization",
+    status: "Online",
+    joiningDate: "14/06/21"
+  },
+  {
+    fullName: "Alexa Liras",
+    email: "alexa@simmmple.com",
+    role: "Programmer",
+    level: "Developer",
+    status: "Offline",
+    joiningDate: "14/06/21"
+  },
+  {
+    fullName: "Esthera Jackson",
+    email: "esthera@simmmple.com",
+    role: "Manager",
+    level: "Organization",
+    status: "Online",
+    joiningDate: "14/06/21"
+  },
+  {
+    fullName: "Alexa Liras",
+    email: "alexa@simmmple.com",
+    role: "Programmer",
+    level: "Developer",
+    status: "Offline",
+    joiningDate: "14/06/21"
+  },
+  
+];
 
 const TablesScreen = () => {
   return (
     <div className="tables-screen">
       <div>
-        <Table renderHeader={renderAutherTableHeader} renderColumnHeader={renderAutherTableColumnHeaders} />
+        <Table items={authors} renderHeader={renderAutherTableHeader} renderColumnHeader={renderAutherTableColumnHeaders} renderRow={renderAutherTableRow} />
       </div>
       <PageFooter className="footer-with-vertical-gap" />
     </div>
