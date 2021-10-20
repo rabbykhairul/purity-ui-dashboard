454de5cbf674d146a76fccba0cadb2e63ad4a462
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 07:58:16 +0600

    add routing for '/login' & '/register' pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { Switch, Route } from "react-router";
+import MainPage from "./components/MainPage";
+import SignInScreen from "./components/SignInScreen";
+import SignUpScreen from "./components/SignUpScreen";
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <Switch>
+      <Route path="/login">
+        <SignInScreen />
+      </Route>
+      <Route path="/register">
+        <SignUpScreen />
+      </Route>
+    </Switch>
   );
 }
 
