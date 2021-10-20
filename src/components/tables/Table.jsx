ba05ee6e47eba20dd323470d65c020c67b4ab786
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 15:17:43 +0600

    make the 'Table' component dynamic

--- src/components/tables/Table.jsx
+++ src/components/tables/Table.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Table = (props) => {
-  const { renderHeader, renderColumnHeader } = props;
+  const { items, renderHeader, renderColumnHeader, renderRow } = props;
 
   return (
     <div className="table-container page-section bg-white">
       {renderHeader && renderHeader()}
       <table>
         {renderColumnHeader()}
+        {items.map((item, idx) => renderRow(item, idx))}
       </table>
     </div>
   )
