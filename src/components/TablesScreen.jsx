8a563a29ea06dc83a1ddda2ceaca9bd671603da2
rabbykhairul <rabbykhairul@gmail.com>
2021-10-20 11:10:01 +0600

    create 'TablesScreen' component

---
+++ src/components/TablesScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageFooter from "./commons/PageFooter";
+
+const TablesScreen = () => {
+  return (
+    <div className="tables-screen">
+      <div></div>
+      <PageFooter className="footer-with-vertical-gap" />
+    </div>
+  );
+};
+
+export default TablesScreen;
