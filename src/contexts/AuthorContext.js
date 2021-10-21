97326c1e27be3cad13e4b492fc5d4f5cd071a50a
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 04:58:12 +0600

    AuthorContext - add method to set selected author

--- src/contexts/AuthorContext.js
+++ src/contexts/AuthorContext.js
@@ -2,6 +2,8 @@ import { createContext } from "react";
 
 const AuthorContext = createContext({
   authors: [],
+  selectedAuthor: null,
+  authorSelected: (author) => author,
   authorCreated: (author) => author,
   authorUpdated: (author) => author,
   authorDeleted: (author) => author,
