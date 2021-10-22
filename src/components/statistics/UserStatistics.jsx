e063d7e3e6115b0dfb881bc03d779f3aa5341dc6
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 18:07:20 +0600

    UserStatistics - display user graph

--- src/components/statistics/UserStatistics.jsx
+++ src/components/statistics/UserStatistics.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { IoWallet } from "react-icons/io5";
 import { IoIosRocket } from "react-icons/io";
 import { MdShoppingCart } from "react-icons/md";
@@ -29,13 +30,65 @@ const ITEMS = [
     percentageIncreased: 60,
     icon: FaWrench
   }
-]
+];
+
+const userData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const UserStatistics = () => {
 
   const renderGraph = () => {
     return (
-      <div className="user-graph-container"></div>
+      <div className="user-graph-container">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart width={150} height={40} data={userData}>
+            <YAxis tick={{ fill: "white", fontSize: "0.65vw" }} />
+            <Bar dataKey="uv" fill="white" barSize={6.5} />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     )
   }
 
