2f36818a60346132cf36d26badc45904a1b07bf4
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 16:14:04 +0600

    Projects table - make the edit & add new buttons functional

--- src/components/TablesScreen.jsx
+++ src/components/TablesScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import AuthorContext from "../contexts/AuthorContext";
+import ProjectContext from "../contexts/ProjectContext";
 import { renderAutherTableColumnHeaders, renderAutherTableHeader, renderAutherTableRow, renderProjectTableColumnHeaders, renderProjectTableHeader, renderProjectTableRow } from "../utils/tableHelper";
 import PageFooter from "./commons/PageFooter";
 import Table from "./tables/Table";
@@ -75,12 +76,13 @@ const projects = [
 
 const TablesScreen = () => {
   const { authors, authorSelected, createNewAuthor } = useContext(AuthorContext);
+  const { projects, projectSelected, createNewProject } = useContext(ProjectContext);
 
   return (
     <div className="tables-screen">
       <div>
         <Table items={authors} renderHeader={renderAutherTableHeader} onAddNewClick={createNewAuthor} renderColumnHeader={renderAutherTableColumnHeaders} renderRow={renderAutherTableRow} onRowClick={authorSelected} />
-        <Table items={projects} renderHeader={renderProjectTableHeader} renderColumnHeader={renderProjectTableColumnHeaders} renderRow={renderProjectTableRow} />
+        <Table items={projects} renderHeader={renderProjectTableHeader} onAddNewClick={createNewProject} renderColumnHeader={renderProjectTableColumnHeaders} renderRow={renderProjectTableRow} onRowClick={projectSelected} />
       </div>
       <PageFooter className="footer-with-vertical-gap" />
     </div>
