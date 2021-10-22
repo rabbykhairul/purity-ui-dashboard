a543ffd255b4899050da2a0e6bb57f6b40f4b0e3
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 19:33:08 +0600

    ProjectForm - add support for project logo upload

--- src/components/forms/ProjectForm.jsx
+++ src/components/forms/ProjectForm.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { FaDatabase, FaTrash } from "react-icons/fa";
 import Input from "../commons/Input";
+import FileInput from "../commons/FileInput";
 import Button from "../commons/Button";
 import ProjectContext from "../../contexts/ProjectContext";
 import { createProject, deleteProject, updateProject } from "../../services/projectService";
@@ -13,11 +14,24 @@ const ProjectForm = () => {
   const [budget, setBudget] = useState(selectedProject?.budget || 0);
   const [status, setStatus] = useState(selectedProject?.status || "");
   const [completionPercentage, setCompletionPercentage] = useState(selectedProject?.completionPercentage || 0);
+  const [projectLogo, setProjectLogo] = useState(null);
 
   const validateFormData = () => title;
 
+  const generatePayload = () => {
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("budget", budget);
+    formData.append("status", status);
+    formData.append("completionPercentage", completionPercentage);
+    if (projectLogo) formData.append("projectLogo", projectLogo, projectLogo?.name);
+
+    return formData;
+  }
+
   const updateProjectDetails = async () => {
-    const payload = { title, budget, status, completionPercentage };
+    const payload = generatePayload();
     const updatedProject = await updateProject(selectedProject._id, payload)
     if (updatedProject) {
       projectUpdated(updatedProject);
@@ -25,7 +39,7 @@ const ProjectForm = () => {
   }
 
   const createNewProject = async () => {
-    const payload = { title, budget, status, completionPercentage };
+    const payload = generatePayload();
     const newProject = await createProject(payload)
 
     if (newProject) projectCreated(newProject);
@@ -52,6 +66,7 @@ const ProjectForm = () => {
       <Input title="Budget" name="budget" placeholder="Enter project budget" type="number" value={budget} onChange={(e) => setBudget(e.target.value)} min="0" />
       <Input title="Status" name="status" placeholder="eg. Working" value={status} onChange={(e) => setStatus(e.target.value)} />
       <Input title="Project progress(0% -100%)" name="completionPercentage" placeholder="eg. 10" type="number" value={completionPercentage} onChange={(e) => setCompletionPercentage(e.target.value)} min="0" max="100" />
+      <FileInput id="project-logo" className="file-input" displayTitle={projectLogo ? projectLogo.name : "Please select a project logo"} accept="image/*" onChange={(e) => setProjectLogo(e.target?.files?.[0])} />
       <div className="btn-container">
         {selectedProject && 
           <Button className="bg-red-500" onClick={handleDelete} >
